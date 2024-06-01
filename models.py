c7702a7617490d3840ca182ba1d08243746b88c5
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-01 21:35:45 +0330

    refactor: Change identity to autoincrement

--- models.py
+++ models.py
@@ -5,6 +5,6 @@ from database import Base
 class Scores(Base):
     __tablename__ = "Scores"
 
-    id = Column(Integer, primary_key=True, identity=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
     score = Column(Integer)
