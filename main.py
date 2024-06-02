0c946a921028405486ae34138e11f86c1fc441c5
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-02 12:54:34 +0330

    feat: Add Cors and import required files

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+import models
+from sqlalchemy.orm import Session
+from sqlalchemy import desc
+from database import SessionLocal, engine, Base, get_db
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Annotated, ClassVar, List
+from schemas import input
 
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"hello":"world"}
+origins = ["http://localhost:*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+db_dependency = Annotated[Session, Depends(get_db())]
