642bddf7ff172ae84d7b3c6ebe35f4855e467dc2
abolfazmz81 <abolfazmz81@gmail.com>
2024-06-02 12:55:39 +0330

    feat: Add update score api

--- main.py
+++ main.py
@@ -20,3 +20,47 @@ app.add_middleware(
 )
 
 db_dependency = Annotated[Session, Depends(get_db())]
+
+
+@app.post("/scores")
+async def update_score(json: List[input], db: Session = Depends(get_db)):
+    user1 = json[0]
+    user2 = json[1]
+    dt1 = db.query(models.Scores).filter(models.Scores.name == json[0].name).first()
+    dt2 = db.query(models.Scores).filter(models.Scores.name == user2.name).first()
+    if not dt1:
+        if json[0].status == "WIN":
+            db_user1 = models.Scores(score=1, name=user1.name)
+        else:
+            db_user1 = models.Scores(score=-1, name=user1.name)
+        db.add(db_user1)
+        db.commit()
+        db.refresh(db_user1)
+    else:
+        if json[0].status == "WIN":
+            dt1.score = dt1.score + 1
+            db.commit()
+            db.refresh(dt1)
+        else:
+            dt1.score = dt1.score - 1
+            db.commit()
+            db.refresh(dt1)
+
+    if not dt2:
+        if json[1].status == "Win":
+            db_user2 = models.Scores(score=1, name=user2.name)
+        else:
+            db_user2 = models.Scores(score=-1, name=user2.name)
+        db.add(db_user2)
+        db.commit()
+        db.refresh(db_user2)
+    else:
+        if json[1].status == "Win":
+            dt2.score = dt2.score + 1
+            db.commit()
+            db.refresh(dt2)
+        else:
+            dt2.score = dt2.score - 1
+            db.commit()
+            db.refresh(dt2)
+    return "ok"
